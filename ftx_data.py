91692537f263795c8f59b1653c3d62a5cb79720b
oriHab <orielh@anyvision.co>
2020-07-22 08:59:48 +0300

    ftx_data.py: logs markets data from ws

---
+++ ftx_data.py
@@ -0,0 +1,55 @@
+import json
+import time
+
+import requests
+import websockets
+import asyncio
+import websocket
+
+REST_EP = 'https://ftx.com/api/'
+WS_EP = "wss://ftx.com/ws/"
+
+
+def send_ping(ws):
+    ws.send(json.dumps({'op': 'ping'}))
+
+
+def get_markets():
+    res = requests.get(f'{REST_EP}/markets')
+    assert res.status_code == 200
+    res_json = res.json()
+    markets = [entry['name'] for entry in res_json['result']]
+    return markets
+
+
+def subscribe_to_markets(ws, markets):
+    for market in markets:
+        ws.send(json.dumps({'op': 'subscribe', 'channel': 'trades', 'market': market}))
+
+
+def process_received_data(ws):
+    while True:
+        response = json.loads(ws.recv())
+        if response['type'] == 'subscribed':
+            continue
+        if response['type'] == 'pong':
+            continue
+        if response['type'] == 'update':
+            for response_data in response['data']:
+                print(
+                    f"{int(time.time())}, FTX, {response['market']}, {response_data['price']}, {response_data['size']}")
+        ws.send(json.dumps({'op': 'ping'}))
+
+
+if __name__ == '__main__':
+    markets = get_markets()
+
+    ws = websocket.WebSocket()
+    ws.connect(f'{WS_EP}/markets')
+    ws.send(json.dumps({'op': 'ping'}))
+    msg = ws.recv()
+    # TODO: send_ping every 5 seconds on secondary thread
+
+    subscribe_to_markets(ws, markets)
+
+    process_received_data(ws)
